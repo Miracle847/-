a4f1baf7db6365229fdc0216eda5dc792e101714
Miracle847 <2678988649@qq.com>
2021-04-29 11:40:36 +0800

    '工程初始化'

---
+++ src/App.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id="app">
+    <!--3、使用子组件-->
+    <el-container>
+      <el-header id="headerhello">
+        <Header/>
+      </el-header>
+      <el-main id="mainhello">
+        <!-- <Login/> -->
+        <router-view></router-view>
+      </el-main>
+      <el-footer id="footerhello">
+        <Footer/>
+      </el-footer>
+    </el-container>
+    
+  </div>
+    
+</template>
+
+<script>
+//1、导入Header 子组件
+import Header from "./components/Header.vue"
+import Login from "./components/Login.vue"
+import Footer from "./components/Footer.vue"
+
+//2、注册  子组件
+export default {
+  name: 'App',
+  components: {
+  Header,
+  Login,
+  Footer
+  }
+}
+</script>
+
+<style>
+*{
+  margin: 0;
+  padding: 0;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  /* text-align: center; */
+  color: #2c3e50;
+  /* margin-top: 60px; */
+}
+#headerhello{
+  background-color: #244152;
+}
+#mainhello{
+  /* background-color: blue; */
+  height: 500px;
+}
+#footerhello{
+  /* background-color: green; */
+  height: 150px !important;
+}
+</style>
---
+++ src/components/Register.vue
@@ -0,0 +1,72 @@
+<template>
+  <div id="loginWrapper">
+    <div id="loginContent">
+      <el-card class="box-card">
+        <div slot="header" class="clearfix">
+          <span>用户注册</span>
+        </div>
+        <div id="loginContentBody">
+          <el-input id="username" v-model="user.username" clearable></el-input>
+          <el-input
+            id="password"
+            v-model="user.password"
+            show-password
+            clearable
+          ></el-input>
+        </div>
+        <div id="loginSubmit">
+          <el-button id="systemSubmit" @click="loginSystem">注册</el-button>
+        </div>
+      </el-card>
+    </div>
+  </div>
+</template>
+
+<script>
+import Axios from "axios";
+export default {
+  name: "register",
+  data() {
+    return {
+      user: {
+        user: {
+          username: "",
+          password: "",
+        },
+      },
+    };
+  },
+  methods: {
+    loginSystem() {
+      Axios.get('http://localhost:3000/register',{
+          params:this.username
+      }).then(function (response) {
+        alert(response.data);
+      });
+    },
+  },
+};
+</script>
+
+<style>
+#loginWrapper {
+  /* background-color: red; */
+  height: 100%;
+}
+#loginContent {
+  width: 400px;
+  margin: 0 auto;
+  padding-top: 100px;
+}
+#systemSubmit {
+  display: block;
+  margin: 0 auto;
+  background-color: #0a9588;
+  color: white;
+  border-color: #0a9588;
+}
+#password {
+  margin-top: 5px;
+  margin-bottom: 8px;
+}
+</style>
